50b6428847c7e780db372081d1e65d5f513eb4f1
Roberto <roberto.geiss.nh@icloud.com>
2024-02-21 20:13:24 -0300

    Initial Commit

---
+++ testeCoordinator2/testeCoordinator2App.swift
@@ -0,0 +1,17 @@
+//
+//  testeCoordinator2App.swift
+//  testeCoordinator2
+//
+//  Created by Roberto Edgar Geiss on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct testeCoordinator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
